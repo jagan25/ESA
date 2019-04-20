f768e419ee6de308e436a70db7bca783fb10e899
Jagan <jagandx@gmail.com>
2019-04-19 20:08:05 -0400

    Adding files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# set :application, "my_app_name"
+# set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -37,3 +37,8 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :application, "esa"
+set :repo_url, "git@github.com:jagan25/ESA.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rvm_ruby_version, '2.3'
+set :passenger_restart_with_touch, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,12 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+role :app, %w{deploy_user@ec2-18-212-107-222.compute-1.amazonaws.com}
+role :web, %w{deploy_user@ec2-18-212-107-222.compute-1.amazonaws.com}
+role :db,  %w{deploy_user@ec2-18-212-107-222.compute-1.amazonaws.com}
+set :ssh_options, {
+   keys: %w(/Users/jagan/Desktop/AWS key pair/esa.pem),
+   forward_agent: false,
+   auth_methods: %w(publickey password)
+ }
