62817dd520813c0f384fdded689e4c4147f882b7
Jagan <jagandx@gmail.com>
2019-04-20 02:18:38 -0400

    test

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-# aws:
-#   access_key_id: 123
-#   secret_access_key: 345
-
-# Used as the base secret for all MessageVerifiers in Rails, including the one protecting cookies.
